3add5ce427f4e4a1f874a81edcb8b2bd431c1ebd
tolentinoel <rckrleyn@gmail.com>
2020-11-27 12:26:30 -0800

    commit tree 63f180f23ede79125781ac9b3fcc8174be67f284

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+  let answer = record.find(obj => obj.result === "W")
+  // return answer ? answer.year : answer
+  if (answer){
+    return answer.year
+  } else {
+    return answer
+  }
+}
+
